534408b8141d1d44532e5c964a0ad80c180b8b92
syidmen <rasyidmuh0907@gmail.com>
2025-06-24 20:23:47 +0700

    added query 4

---
+++ project_sql/4_top_skills_by_salary.sql
@@ -0,0 +1,120 @@
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg),0) AS average_salary
+FROM job_postings_fact
+JOIN skills_job_dim
+    USING (job_id)
+JOIN skills_dim
+    USING (skill_id)
+WHERE 
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+GROUP BY skills
+ORDER BY average_salary DESC
+LIMIT 25
+
+/*
+[
+  {
+    "skills": "pyspark",
+    "average_salary": "208172"
+  },
+  {
+    "skills": "bitbucket",
+    "average_salary": "189155"
+  },
+  {
+    "skills": "couchbase",
+    "average_salary": "160515"
+  },
+  {
+    "skills": "watson",
+    "average_salary": "160515"
+  },
+  {
+    "skills": "datarobot",
+    "average_salary": "155486"
+  },
+  {
+    "skills": "gitlab",
+    "average_salary": "154500"
+  },
+  {
+    "skills": "swift",
+    "average_salary": "153750"
+  },
+  {
+    "skills": "jupyter",
+    "average_salary": "152777"
+  },
+  {
+    "skills": "pandas",
+    "average_salary": "151821"
+  },
+  {
+    "skills": "elasticsearch",
+    "average_salary": "145000"
+  },
+  {
+    "skills": "golang",
+    "average_salary": "145000"
+  },
+  {
+    "skills": "numpy",
+    "average_salary": "143513"
+  },
+  {
+    "skills": "databricks",
+    "average_salary": "141907"
+  },
+  {
+    "skills": "linux",
+    "average_salary": "136508"
+  },
+  {
+    "skills": "kubernetes",
+    "average_salary": "132500"
+  },
+  {
+    "skills": "atlassian",
+    "average_salary": "131162"
+  },
+  {
+    "skills": "twilio",
+    "average_salary": "127000"
+  },
+  {
+    "skills": "airflow",
+    "average_salary": "126103"
+  },
+  {
+    "skills": "scikit-learn",
+    "average_salary": "125781"
+  },
+  {
+    "skills": "jenkins",
+    "average_salary": "125436"
+  },
+  {
+    "skills": "notion",
+    "average_salary": "125000"
+  },
+  {
+    "skills": "scala",
+    "average_salary": "124903"
+  },
+  {
+    "skills": "postgresql",
+    "average_salary": "123879"
+  },
+  {
+    "skills": "gcp",
+    "average_salary": "122500"
+  },
+  {
+    "skills": "microstrategy",
+    "average_salary": "121619"
+  }
+]
+*/
